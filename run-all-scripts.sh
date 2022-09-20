1a383d6e1bf57ade9dc1d9870d32e864017e5727
Flavio Fernandes <93294315+flavio-fds@users.noreply.github.com>
2022-09-20 11:13:09 -0300

    Add files via upload

---
+++ run-all-scripts.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+chmod +x ./install-packages/script-aur.sh
+chmod +x ./install-packages/software-pacman-base.sh
+chmod +x ./install-packages/software-pacman.sh
+
+chmod +x ./config-system/script-config-docker.sh
+chmod +x ./config-system/script-config-general.sh
+chmod +x ./config-system/script-config-github.sh
+chmod +x ./config-system/script-config-nvm.sh
+chmod +x ./config-system/script-config-zsh.sh
+
+chmod +x ./install-archlinux/install-archlinux.sh
+
+
+
+
